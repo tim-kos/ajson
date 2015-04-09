ac27a4c8092388f70d8a48a05f5927af6eb25179
tim-kos <tim@debuggable.com>
2015-04-09 22:19:59 +0200

    adding more benchmarks

--- bench.js
+++ bench.js
@@ -7,10 +7,11 @@ blocked(function(ms) {
   loopBlockedFor += ms;
 });
 
-var timePerTest = 2 * 1000;
+var timePerTest = 1 * 1000;
 var seconds     = (timePerTest / 1000);
 
-var dataToParse = "{\"a\":\"b\",\"c\":\"d\"}";
+var dataToParse     = "{\"a\":\"b\",\"c\":\"d\"}";
+var dataToStringify = {a:"b",c:"d"};
 
 function testJsonParse(cb) {
   loopBlockedFor = 0;
@@ -56,6 +57,50 @@ function testAjsonParse(cb) {
   );
 }
 
+function testJsonStringify(cb) {
+  loopBlockedFor = 0;
+
+  var now = +new Date();
+  var i = 0;
+  while (true) {
+    var parsed = JSON.stringify(dataToStringify);
+    i++;
+
+    var now2 = +new Date();
+
+    if (now2 - now > timePerTest) {
+      break;
+    }
+  }
+  var end = +new Date();
+  loopBlockedFor = end - now;
+
+  cb(null, i);
+}
+
+function testAjsonStringify(cb) {
+  loopBlockedFor = 0;
+
+  var now = +new Date();
+  var i   = 0;
+
+  async.whilst(
+    function() {
+      var now2 = +new Date();
+      return now2 - now <= timePerTest;
+    },
+    function (callback) {
+      AJSON.stringify(dataToStringify, function(err, parsed) {
+        i++;
+        callback();
+      });
+    },
+    function (err) {
+      cb(null, i);
+    }
+  );
+}
+
 async.series(
   [
     function(callback) {
@@ -77,6 +122,28 @@ async.series(
         }
 
         console.log(howMany + ' AJSON.parse()s in ' + seconds + 's, ' + (loopBlockedFor / 1000) + 's event loop blocked');
+        callback();
+      });
+    },
+    function(callback) {
+      console.log('Running JSON.stringify benchmark');
+      testJsonStringify(function(err, howMany) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(howMany + ' JSON.stringify()s in ' + seconds + 's, ' + (loopBlockedFor / 1000) + 's event loop blocked');
+        callback();
+      });
+    },
+    function(callback) {
+      console.log('Running AJSON.stringify benchmark');
+      testAjsonStringify(function(err, howMany) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(howMany + ' AJSON.stringify()s in ' + seconds + 's, ' + (loopBlockedFor / 1000) + 's event loop blocked');
         callback();
       });
     },
